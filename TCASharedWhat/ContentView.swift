4931b6017180c4b0a679a01778c317cdba603281
Alex Antonyuk <oantoniuk-ecw@cricut.com>
2024-10-17 14:19:00 +0300

    ok

--- TCASharedWhat/ContentView.swift
+++ TCASharedWhat/ContentView.swift
@@ -5,20 +5,120 @@
 //  Created by oantoniuk on 17.10.2024.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
+@Reducer
+struct Feature {
+    struct State {
+        @Shared var thing: SharedThing
+
+        init(thing: SharedThing = .init()) {
+            self._thing = Shared(wrappedValue: .init(), .sharedThing)
+        }
+    }
+
+    enum Action {
+        case onAppear
+        case toggleThing
+        case sharedDidChange
+    }
+
+    enum CancelID {
+        case sharedState
+    }
+
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .onAppear:
+                if #available(iOS 17.0, *) {
+                    return .none
+                } else {
+                    return .run { [thing = state.$thing] send in
+                        for await _ in thing.publisher.values {
+                            await send(.sharedDidChange)
+                        }
+                    }
+                    .cancellable(id: CancelID.sharedState, cancelInFlight: true)
+                }
+
+            case .toggleThing:
+                state.thing.isOk.toggle()
+                return .none
+
+            case .sharedDidChange:
+                return .none
+            }
+        }
+    }
+}
+
 struct ContentView: View {
+    struct ViewState: Equatable {
+        let isOn: Bool
+
+        init(state: Feature.State) {
+            self.isOn = state.thing.isOk
+        }
+    }
+
+    let store: StoreOf<Feature>
+    @ObservedObject var viewStore: ViewStore<ViewState, Feature.Action>
+
+    init(store: StoreOf<Feature>) {
+        self.store = store
+        self.viewStore = .init(store, observe: ViewState.init(state:))
+    }
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Toggle(isOn: viewStore.binding(get: \.isOn, send: { _ in .toggleThing })) {
+                Text("Is OK?")
+            }
         }
         .padding()
+        .onAppear {
+            viewStore.send(.onAppear)
+        }
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(store: Store(initialState: Feature.State(), reducer: {
+        Feature()
+    }))
+}
+
+/// ---------------
+
+struct SharedThing {
+    var isOk: Bool
+
+    init(isOk: Bool = false) {
+        self.isOk = isOk
+    }
+}
+
+extension PersistenceReaderKey where Self == PersistenceKeyDefault<InMemoryKey<SharedThing>> {
+    static var sharedThing: Self {
+        PersistenceKeyDefault(.inMemory("sharedThing"), SharedThing())
+    }
+}
+
+public protocol IOS16SharedStateAction: Equatable {
+    static var _sharedStateDidUpdate: Self { get }
+}
+
+extension Effect where Action: IOS16SharedStateAction {
+    public static func syncSharedState<Value>(_ state: Shared<Value>) -> Effect<Action> {
+        .run { send in
+            for await _ in state.publisher.values {
+                await send(Action._sharedStateDidUpdate)
+            }
+        }
+    }
 }
--- TCASharedWhat/TCASharedWhatApp.swift
+++ TCASharedWhat/TCASharedWhatApp.swift
@@ -5,13 +5,29 @@
 //  Created by oantoniuk on 17.10.2024.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct TCASharedWhatApp: App {
+
+    init() {
+        Task {
+            try await Task.sleep(for: .seconds(1))
+            @Shared(.sharedThing) var thing
+
+            while (true) {
+                thing.isOk.toggle()
+                try await Task.sleep(for: .seconds(2))
+            }
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(store: Store(initialState: Feature.State(), reducer: {
+                Feature()
+            }))
         }
     }
 }
