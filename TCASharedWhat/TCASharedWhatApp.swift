cf3bb8c50c7f4c62b03414f2352e56ed0c91235c
Alex Antonyuk <oantoniuk-ecw@cricut.com>
2024-10-17 14:02:28 +0300

    Initial Commit

---
+++ TCASharedWhat/TCASharedWhatApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCASharedWhatApp.swift
+//  TCASharedWhat
+//
+//  Created by oantoniuk on 17.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct TCASharedWhatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
